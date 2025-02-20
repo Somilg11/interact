fca25ab6cfacbd0021c71d193f51b1a9f2f1061c
Somil Gupta <gsomil93@gmail.com>
2025-02-20 12:08:20 +0530

    added navbar, theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,11 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 
 
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      home
     </div>
   );
 }
