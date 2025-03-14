261a5fe7655f618e0641efda36c33854b06351bc
Somil Gupta <gsomil93@gmail.com>
2025-03-14 23:25:53 +0530

    minor type issue

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { clsx, type ClassValue } from "clsx";
 import { addHours, intervalToDuration, isAfter, isBefore, isWithinInterval } from "date-fns";
 import { twMerge } from "tailwind-merge";
