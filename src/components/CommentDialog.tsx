3fd035d2a5647b1d29fca620eecb99635d611656
Somil Gupta <gsomil93@gmail.com>
2025-03-14 23:37:44 +0530

    type issues fixed

--- src/components/CommentDialog.tsx
+++ src/components/CommentDialog.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState } from "react";
 import { Id } from "../../convex/_generated/dataModel";
 import { useMutation, useQuery } from "convex/react";
